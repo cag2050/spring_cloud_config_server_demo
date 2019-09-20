b0b9661245144f5a9dce74a29449ecd1a70b081a
chen_anguo <cag2050@163.com>
2019-09-20 20:21:38 +0800

    project init

---
+++ src/main/java/com/example/spring_cloud_config_server_demo/SpringCloudConfigServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_cloud_config_server_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_cloud_config_server_demo/SpringCloudConfigServerDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.spring_cloud_config_server_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudConfigServerDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
